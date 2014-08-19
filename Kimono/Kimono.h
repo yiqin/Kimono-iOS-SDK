2a8a46428c785ad075583a22679f273b46fe3ec6
yiqin <yiqin.mems@gmail.com>
2014-08-18 20:07:10 -0400

    Init Kimono.h and add AFNetworking.

---
+++ Kimono/Kimono.h
@@ -0,0 +1,13 @@
+//
+//  Kimono.h
+//  Kimono-iOS-SDK
+//
+//  Created by yiqin on 8/18/14.
+//  Copyright (c) 2014 yiqin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Kimono : NSObject
+
+@end
---
+++ Kimono/Kimono.m
@@ -0,0 +1,13 @@
+//
+//  Kimono.m
+//  Kimono-iOS-SDK
+//
+//  Created by yiqin on 8/18/14.
+//  Copyright (c) 2014 yiqin. All rights reserved.
+//
+
+#import "Kimono.h"
+
+@implementation Kimono
+
+@end
